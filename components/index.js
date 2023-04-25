02b40797bfdf991245378fe65b05f4ef89af747a
Sharkjr17 <120116657+Sharkjr17@users.noreply.github.com>
2023-04-25 20:34:34 +0000

    modified:   components/index.css 	modified:   components/index.js 	modified:   index.html

--- components/index.js
+++ components/index.js
@@ -1,1 +1,32 @@
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+setInterval(function() { updateStats(); }, 20);
+
+
+
+var cash = 100;
+var MM = 0;
+var MB = 0;
+
+
+
+
+function updateStats() {
+document.getElementById("cashdisplay").innerHTML = "Cash: " + cash;
+
+
+
+}
+
+
+
+
+
+
+
+
+function buyMM() {
+    MM += 1;
+}
+
+
+
+
