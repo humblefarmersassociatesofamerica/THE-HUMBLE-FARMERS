bb12d827a07431ade72dfc9af8d2a0a7bf28c0b3
Sharkjr17 <120116657+Sharkjr17@users.noreply.github.com>
2023-04-26 20:08:43 +0000

    modified:   components/index.css 	modified:   components/index.js 	modified:   index.html

--- components/index.js
+++ components/index.js
@@ -11,8 +11,8 @@ var MB = 0;
 
 function updateStats() {
 document.getElementById("cashdisplay").innerHTML = "Cash: " + cash;
-
-
+document.getElementById("MMdisplay").innerHTML = "Mystery Meat: " + MM;
+document.getElementById("MBdisplay").innerHTML = "Moldy Buns: " + MB;
 
 }
 
@@ -23,8 +23,20 @@ document.getElementById("cashdisplay").innerHTML = "Cash: " + cash;
 
 
 
-function buyMM() {
-    MM += 1;
+function scavenge() {
+    const a = Math.floor(Math.random() * 101);
+    if(a >= 66.66666) 
+    { 
+        MM += 1;
+    } 
+    else if(a >= 33.33333)
+    { 
+        MB += 1;  
+    }
+    else if(a >= 0)
+    {
+        cash += 1;
+    }
 }
 
 
