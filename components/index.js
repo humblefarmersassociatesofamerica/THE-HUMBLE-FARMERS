b0b79a36b00671224001bff3916ee8eef8d9ee38
Sharkjr17 <120116657+Sharkjr17@users.noreply.github.com>
2023-04-28 04:34:22 +0000

    modified:   components/index.js 	modified:   index.html

--- components/index.js
+++ components/index.js
@@ -1,27 +1,47 @@
-if(typeof(storage) == "undefined")  { alert("Warning " + " your browser does not support local save, when you close this browser all progress will be lost")}
+/* start of save functions*/
+if(typeof(storage) == "undefined")  { alert("Warning " + " your browser does not support local save, when you close this browser all progress will be lost");}
 setInterval(function() {save()}, 20);
 
 function save() {
-    localStorage.cashCount = cash;
+    number(localStorage.cashCount) = cash;
+    number(localStorage.MysteryMeat) = MM;
+    number(localStorage.MoldyBread) = MB;
 }
 
-if (typeof(storage) !== "undefined" || localStorage.cashCount !== "undefined") {
+function clearSave() {
+    localStorage.clear();
+    if(localStorage.length === 0)
+    cash = 0;
+    MM = 0;
+    MB = 0;
+    alert("ahhhh");
+ }
+
+if (typeof(storage) !== "undefined" || localStorage.length > 0) {
     var cash = localStorage.cashCount;
+    var MM = localStorage.MysteryMeat;
+    var MB = localStorage.MoldyBread;
+    alert("pain");
 }
-else
-{
-    var cash = 0;
-    var MM = 0;
-    var MB = 0;
-}
+else { alert("no save found"); }
+
+/* end of save functions*/
 
 
-
+/* start of variables and arrays*/
+var cash = 0;
+var MM = 0;
+var MB = 0;
 
 
 
 const log = ["Welcome to insert name here LLLLL"];
+/* end of variables and arrays*/
 
+
+
+
+/* start update stat functions */
 setInterval(function() { updateStats(); }, 20);
 
 function updateStats() {
@@ -33,10 +53,12 @@ document.getElementById("messageLog").innerHTML = log.join(" <br> ");
 
 if(log.length > 30) { log.pop();}
 }
+/* end update stat functions */
 
 
 
 
+/* start of the game */
 function scavenge() {
     const a = Math.floor(Math.random() * 101);
     if(a >= 50) 
