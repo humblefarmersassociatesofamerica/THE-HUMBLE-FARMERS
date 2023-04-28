a22536b216d81a1f2b333cb4e2abc921a2345e68
Sharkjr17 <120116657+Sharkjr17@users.noreply.github.com>
2023-04-28 02:11:07 +0000

    modified:   components/index.css 	modified:   components/index.js 	modified:   index.html

--- components/index.js
+++ components/index.js
@@ -1,19 +1,18 @@
-setInterval(function() { updateStats(); }, 20);
+if(typeof(storage) == "undefined")  { alert("Warning " + " your browser does not support local save, when you close this browser all progress will be lost")}
+setInterval(function() {save()}, 20);
 
+function save() {
+    localStorage.cashCount = cash;
+}
 
-
-var cash = 100;
-var MM = 0;
-var MB = 0;
-
-
-
-
-function updateStats() {
-document.getElementById("cashdisplay").innerHTML = "Cash: " + cash;
-document.getElementById("MMdisplay").innerHTML = "Mystery Meat: " + MM;
-document.getElementById("MBdisplay").innerHTML = "Moldy Buns: " + MB;
-
+if (typeof(storage) !== "undefined" || localStorage.cashCount !== "undefined") {
+    var cash = localStorage.cashCount;
+}
+else
+{
+    var cash = 0;
+    var MM = 0;
+    var MB = 0;
 }
 
 
@@ -21,21 +20,40 @@ document.getElementById("MBdisplay").innerHTML = "Moldy Buns: " + MB;
 
 
 
+const log = ["Welcome to insert name here LLLLL"];
+
+setInterval(function() { updateStats(); }, 20);
+
+function updateStats() {
+document.getElementById("cashdisplay").innerHTML = "Cash: " + cash;
+document.getElementById("MMdisplay").innerHTML = "Mystery Meat: " + MM;
+document.getElementById("MBdisplay").innerHTML = "Moldy Buns: " + MB;
+document.getElementById("messageLog").innerHTML = log.join(" <br> ");
+
+
+if(log.length > 30) { log.pop();}
+}
+
+
 
 
 function scavenge() {
     const a = Math.floor(Math.random() * 101);
-    if(a >= 66.66666) 
+    if(a >= 50) 
     { 
         MM += 1;
+        log.unshift("You Gained 1 Mystery Meat");
     } 
-    else if(a >= 33.33333)
+    else if(a >= 10)
     { 
         MB += 1;  
+        log.unshift("You Gained 1 Moldy Bun")
     }
     else if(a >= 0)
     {
         cash += 1;
+        log.unshift("Its Your Lucky Day! Gained 1 Cash")
+
     }
 }
 
