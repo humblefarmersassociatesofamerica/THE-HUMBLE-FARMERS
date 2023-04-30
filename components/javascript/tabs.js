5e98ab574b867f5dffce92fcb6d9b993b0b3b018
Sharkjr17 <120116657+Sharkjr17@users.noreply.github.com>
2023-04-30 18:14:22 +0000

    modified:   components/css/index.css 	modified:   components/css/tabs.css 	modified:   components/javascript/tabs.js 	modified:   index.html

--- components/javascript/tabs.js
+++ components/javascript/tabs.js
@@ -12,11 +12,16 @@ function openTab(evt, tabName) {
     evt.currentTarget.className += " active";
 }
 
-document.getElementById("defaultOpen").click();
-alert("AHHHH");
-
-document.getElementById("defaultOpen").onload = function(){alert("AHHHH");};
-
-function startup() {
-  alert("AHHHH");
+function openTab2(evt2, tabName2) {
+  var j, tabcontent2, tablinks2;
+  tabcontent2 = document.getElementsByClassName("tabcontent2");
+  for (j = 0; j < tabcontent2.length; j++) {
+    tabcontent2[j].style.display = "none";
+  }
+  tablinks2 = document.getElementsByClassName("tablinks2");
+  for (j = 0; j < tablinks2.length; j++) {
+    tablinks2[j].className = tablinks2[j].className.replace(" active", "");
+  }
+  document.getElementById(tabName2).style.display = "block";
+  evt2.currentTarget.className += " active";
 }
